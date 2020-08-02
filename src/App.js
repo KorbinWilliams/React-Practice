3ddcce92b0555b3dd6df159f6e9d103a9efde534
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-08-02 17:09:33 -0600

    state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/HelloJSX";
 import HelloJs from "./components/HelloJS";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
-      <Greet name="jesus" heroName="jehova">
+      <Message />
+      {/* <Greet name="jesus" heroName="jehova">
         <p>These are the children</p>
       </Greet>
       <Greet name="satan" heroName="diablo">
@@ -17,7 +19,7 @@ function App() {
       <Greet name="budhha" heroName="sidartha gatama"></Greet>
       <Welcome name="jesus" heroName="jehova"></Welcome>
       <Welcome name="satan" heroName="diablo"></Welcome>
-      <Welcome name="budhha" heroName="sidartha gatama"></Welcome>
+      <Welcome name="budhha" heroName="sidartha gatama"></Welcome> */}
     </div>
   );
 }
