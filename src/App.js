b41aa42a8c6b163294f56a1e45e2188232e30534
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-08-02 19:13:51 -0600

    event handlers

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ import Hello from "./components/HelloJSX";
 import HelloJs from "./components/HelloJS";
 import Message from "./components/Message";
 import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
       {/* <Message /> */}
-      <Greet name="jesus" heroName="jehova"></Greet>
+      {/* <Greet name="jesus" heroName="jehova"></Greet> */}
       {/* <Greet name="satan" heroName="diablo">
         <button>Action</button>
       </Greet>
       <Greet name="budhha" heroName="sidartha gatama"></Greet>
       <Welcome name="jesus" heroName="jehova"></Welcome>
   <Welcome name="satan" heroName="diablo"></Welcome>*/}
-      <Welcome name="budhha" heroName="sidartha gatama"></Welcome>
+      {/* <Welcome name="budhha" heroName="sidartha gatama"></Welcome> */}
+      <FunctionClick />
+      <ClassClick />
     </div>
   );
 }
