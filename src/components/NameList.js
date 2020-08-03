3a8e908f23c19459b6db0194170bf408f0056ca4
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-08-03 16:59:52 -0600

    using index as a key

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -9,32 +9,38 @@ class NameList extends Component {
     // return <div>{nameList}</div>;
 
     // NOTE exercise #2 rendering lists(includes person.js)
-    const persons = [
-      {
-        id: 1,
-        name: "Bruce",
-        age: 30,
-        skill: "React",
-      },
-      {
-        id: 2,
-        name: "Clark",
-        age: 25,
-        skill: "Angular",
-      },
-      {
-        id: 3,
-        name: "Diana",
-        age: 42,
-        skill: "Vue",
-      },
-    ];
+    const names = ["Bruce", "Clark", "Diana"];
+    // const persons = [
+    //   {
+    //     id: 1,
+    //     name: "Bruce",
+    //     age: 30,
+    //     skill: "React",
+    //   },
+    //   {
+    //     id: 2,
+    //     name: "Clark",
+    //     age: 25,
+    //     skill: "Angular",
+    //   },
+    //   {
+    //     id: 3,
+    //     name: "Diana",
+    //     age: 42,
+    //     skill: "Vue",
+    //   },
+    // ];
 
-    const personList = persons.map((person) => (
-      <Person key={person.id} person={person} />
+    // const personList = persons.map((person) => (
+    //   <Person key={person.id} person={person} />
+    // ));
+    const nameList = names.map((name, index) => (
+      <h2 key={index}>
+        {name}-{index}
+      </h2>
     ));
 
-    return <div>{personList}</div>;
+    return <div>{nameList}</div>;
   }
 }
 
