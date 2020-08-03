5cbf430fcff9749760b23f91e271f05bca6d59db
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-08-03 16:35:01 -0600

    list rendering

---
+++ src/components/NameList.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Person from "./Person";
+
+class NameList extends Component {
+  render() {
+    // NOTE exercise #1
+    // const names = ["Bruce", "Clark", "Diana"];
+    // const nameList = names.map((n) => <h2>{n}</h2>);
+    // return <div>{nameList}</div>;
+
+    // NOTE exercise #2 rendering lists(includes person.js)
+    const persons = [
+      {
+        id: 1,
+        name: "Bruce",
+        age: 30,
+        skill: "React",
+      },
+      {
+        id: 2,
+        name: "Clark",
+        age: 25,
+        skill: "Angular",
+      },
+      {
+        id: 3,
+        name: "Diana",
+        age: 42,
+        skill: "Vue",
+      },
+    ];
+
+    const personList = persons.map((person) => <Person person={person} />);
+
+    return <div>{personList}</div>;
+  }
+}
+
+export default NameList;
